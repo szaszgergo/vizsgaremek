e5cc58132af2eb8a684057fba965968fbfee94ef
Fr-y <gaborkorpa@gmail.com>
2024-10-09 12:50:22 +0200

    fix

--- actions/naplozas.php
+++ actions/naplozas.php
@@ -5,7 +5,12 @@ $curdate  = date('Y-m-d h:i:s', time());
 $ip = $_SERVER['REMOTE_ADDR'];
 $sessionid = session_id();
 $url = $_SERVER['REQUEST_URI'];
+if(isset($_SESSION["uid"])){
+    $uid = $_SESSION["uid"];
+} else{
+    $uid = '';
+}
 $sql = "INSERT INTO naplo (nID, nDatum, nIP, nSession, nuID, nURL)
-VALUES ('', '$curdate', '$ip', '$sessionid', '1', '$url')";
+VALUES ('', '$curdate', '$ip', '$sessionid', '$uid', '$url')";
 sqlsave($sql);
 }
