bb97e4f0d515e1a6835578790a2e25a5bccaca04
Fr-y <gaborkorpa@gmail.com>
2024-10-10 20:52:06 +0200

    uj apik, adatbazis update, kapcsolódáshoz config fajl implementalasa

--- api/useradatok/index.php
+++ api/useradatok/index.php
@@ -1,37 +1,53 @@
 <?php
-    //userinfo
     header('Content-Type: application/json; charset=utf-8');
+    include("../../actions/sqlcall.php");
+    function getJegyTipusAdatok($tipus){
+        $sql = "SELECT * FROM tipusok WHERE tpID = $tipus";
+        $sor = sqlcall($sql)->fetch_assoc();
+        return $sor;
+    }
+
 
     if( !isset($_GET["uid"]))
     {
-	    $tomb = array( 'hiba' => "hiányos adatok" ,  'uzenet' => "megadandó paraméterek: uid" ) ;
+	    $tomb = array(  'status'    => 400 ,
+                        'hiba'      => "hiányos adatok" ,
+                        'uzenet'    => "megadandó paraméterek: uid" ) ;
     }
     else
     {
-
-        include("../../actions/sqlcall.php");
         $userlekerdezes = "SELECT * FROM user WHERE uID = '$_GET[uid]'";
         $user = sqlcall($userlekerdezes);
         $useradatok = $user->fetch_assoc();
+        if (!isset($useradatok)) {
+            $tomb = array(  'status'    => 404  ,
+                            'hiba'      => "nincs találat" ,
+                            'uzenet'    => "megadott paraméter nem helyes, nem létezik" ) ;
+            }
+            else{
+                $jegyeklekerdezes = "SELECT * FROM jegyek WHERE juID = '$_GET[uid]'";
+                $jegyek = sqlcall($jegyeklekerdezes);
+                $jegyekarray = [];
+                while ($row = $jegyek->fetch_assoc()) {
+                    $jegyekarray[] = $row;
+                }
+                if (sizeof($jegyekarray)) {       
+                    //hozzaadjuk a jegyekhez a nevet is
+                    foreach ($jegyekarray as $key => $value) {
+                        $jegyekarray[$key]["jNev"] = getJegyTipusAdatok($value['jtID'])['tpNev']; 
+                    }
+                }
 
-        $jegyeklekerdezes = "SELECT * FROM jegyek WHERE juID = '$_GET[uid]'";
-        $jegyek = sqlcall($jegyeklekerdezes);
-        $jegyekarray = [];
+                $tomb = array(
+                                'status'        => 200 ,
+                                'adatok'        => $useradatok ,
+                                'jegyek'        => $jegyekarray ,
+                            ) ;
 
-        while ($row = $jegyek->fetch_assoc()) {
-            $jegyekarray[] = $row;
+                }
         }
-    
+        $json = json_encode( $tomb , JSON_UNESCAPED_UNICODE ) ;
 
-
-	$tomb = array(   'adatok'        => $useradatok ,
-	                 'jegyek'        => $jegyekarray ,
-	             ) ;
-
-    }
-
-    $json = json_encode( $tomb , JSON_UNESCAPED_UNICODE ) ;
-
-    print $json ;
+        print $json ;
 
 ?>
