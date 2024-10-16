093c3a84ce2e6cb218d346194aa37680655d2796
Fr-y <gaborkorpa@gmail.com>
2024-10-16 14:41:19 +0200

    nem kötelező képet feltölteni

--- actions/updateaction.php
+++ actions/updateaction.php
@@ -3,13 +3,17 @@ require("sqlcall.php");
 session_start();
 $kepnev=$_SESSION['uid']."_".date("ymdHis")."_".uniqid('', more_entropy: true); //api kell majd
 $kepadat=$_FILES['upic'];
-if($kepadat['type']=='image/jpeg') $kiterj=".jpg";else
-if($kepadat['type']=='image/png') $kiterj=".png";else
-if($kepadat['type']=='image/gif') $kiterj=".gif";else
-die("<script>alert('A kép csak .JPG, vagy .PNG, vagy .GIF lehet!')</script>");
-$kepnev.= $kiterj;
-move_uploaded_file($kepadat['tmp_name'],"../profile_pic/".$kepnev);
-$og_pic=$kepadat['name'];
+$vankep = $_FILES['upic']['size'] > 0;
+if ($vankep) {
+    if($kepadat['type']=='image/jpeg') $kiterj=".jpg";else
+    if($kepadat['type']=='image/png') $kiterj=".png";else
+    if($kepadat['type']=='image/gif') $kiterj=".gif";else
+    die("<script>alert('A kép csak .JPG, vagy .PNG, vagy .GIF lehet!')</script>");
+    $kepnev.= $kiterj;
+    move_uploaded_file($kepadat['tmp_name'],"../profile_pic/".$kepnev);
+    $og_pic=$kepadat['name'];
+}
+
 
 if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["date"])) {
     //form adatok lekérdezési a POST-ból
@@ -17,7 +21,12 @@ if (isset($_POST["username"]) && isset($_POST["email"]) && isset($_POST["date"])
     $date =  $_POST["date"];
 
     //sql insert statement osszerakása
-    $sql = "UPDATE user SET uemail = '$email', uSzuletesidatum = '$date', uProfilePic='$kepnev', uOriginPic='$og_pic' WHERE uid = $_SESSION[uid] ";
+    $sql = "UPDATE user SET uemail = '$email', uSzuletesidatum = '$date'";
+    if ($vankep) {
+        $sql .= ", uProfilePic='$kepnev', uOriginPic='$og_pic' WHERE uid = $_SESSION[uid] ";
+    } else{
+        $sql .=  "WHERE uid = $_SESSION[uid] ";
+    }
     //sql meghivása
     sqlsave($sql);
     }
