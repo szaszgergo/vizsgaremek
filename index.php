5bc5c2204a2250575b311bb71d0a5713a4011d31
odoroliver <scrapyvagyok@gmail.com>
2024-10-24 12:28:31 +0200

    Merge branch 'dev' of https://github.com/szaszgergo/vizsgaremek into dev

--- index.php
+++ index.php
@@ -56,26 +56,31 @@ include('actions/apicall.php');
             <a class="text-end" href="">4222 Budapest, Utca u. 117.</a>
             <a class="text-end" href="">6:00 - 23:00</a>
             <form class="d-flex align-items-center">
-                <?php if (!isset($_SESSION["loggedin"])) {
-                    echo '<a class="btn btn-warning m-2" href="./?o=loginform">Bejelentkezés</a>
-                    <a class="btn btn-warning m-2" href="./?o=registerform">Regisztráció </a>';
-                } else {
-                    include("actions/getuserinfo.php");
-                    include("actions/getjegytipusadatok.php");
-                    
-                    $a = '
+                <?php if (!isset($_SESSION["loggedin"])): ?>
+                    <a class="btn btn-warning m-2" href="./?o=loginform">Bejelentkezés </a>
+                    <a class="btn btn-warning m-2" href="./?o=registerform">Regisztráció</a>
+                <?php else: ?>
+                    <!-- Felhasználói profil információ -->
                     <span class="navbar-text">
                         <a href="./?o=fiok" id="felhasznalo_nev" class="d-flex align-items-center">
-                            '.getUserInfo()[2].'
-                            <span style="padding-left: 0.5rem;"></span>
-                            
-                            <img alt="Profile Image" class="profile-image" src="profile_pic/' . 
-                            (empty(getUserInfo()[11]) ? '../images/pic.png' : getUserInfo()[11]) . '"/>
+                            <?= getUserInfo()[2] ?>
+                            <img alt="Profile Image" class="profile-image" src="profile_pic/<?= empty(getUserInfo()[11]) ? '../images/pic.png' : getUserInfo()[11] ?>"/>
                         </a>
                     </span>
-                    <a href="actions/logout.php" class="btn btn-danger m-2">Kijelentkezés</a>';
-                    echo $a;
-                } ?>
+                    <a href="actions/logout.php" class="btn btn-danger m-2">Kijelentkezés</a>
+                <?php endif; ?>
+                <!-- Hamburger ikon -->
+                <div class="dropdown">
+                    <button class="btn btn-dark dropdown-toggle" type="button" id="hamburgerMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="hamburgerMenu">
+                        <li><a class="dropdown-item" href="./?o=arak">Áraink</a></li>
+                        <li><a class="dropdown-item" href="#">Edzők</a></li>
+                        <li><a class="dropdown-item" href="#">Galéria</a></li>
+                        <li><a class="dropdown-item" href="#">Gyakori kérdések</a></li>
+                    </ul>
+                </div>
             </form>
         </div>
     </nav>
