ad0214cc02b59843dd9212f83a38fa2d17d17488
Fr-y <gaborkorpa@gmail.com>
2024-10-25 20:04:24 +0200

    navbar rendbe rakva + kulon fajlba

--- index.php
+++ index.php
@@ -51,40 +51,8 @@ include('actions/getjegytipusadatok.php');
 <body>
     <div class="site">
         <?php include('actions/naplozas.php'); naplo(); ?>
-        <nav class="navbar navbar-dark  bg-dark p-3">
-            <div class="container-fluid">
-                <a class="navbar-brand text-warning fs-2" href="./"><img src="images/logo.png" alt="LiftZone" title="LiftZone" class="logo"></a>
-                <p id="nav_p" class="text-end" href="">4222 Budapest, Utca u. 117.</p>
-                <p id="nav_p" class="text-end" href="" >6:00 - 23:00</p>
-                <form class="d-flex align-items-center">
-                    <?php if (!isset($_SESSION["loggedin"])): ?>
-                        <a class="btn btn-warning m-2" href="./?o=loginform">Bejelentkezés </a>
-                        <a class="btn btn-warning m-2" href="./?o=registerform">Regisztráció</a>
-                    <?php else: ?>
-                        <!-- Felhasználói profil információ -->
-                        <span class="navbar-text">
-                            <a href="./?o=fiok" id="felhasznalo_nev" class="d-flex align-items-center">
-                                <?= getUserInfo()[2] ?>
-                                <img alt="Profile Image" class="profile-image" src="profile_pic/<?= empty(getUserInfo()[11]) ? '../images/pic.png' : getUserInfo()[11] ?>"/>
-                            </a>
-                        </span>
-                        <a href="actions/logout.php" class="btn btn-danger m-2">Kijelentkezés</a>
-                    <?php endif; ?>
-                    <!-- Hamburger ikon -->
-                    <div class="dropdown">
-                        <button class="btn btn-dark dropdown-toggle" type="button" id="hamburgerMenu" data-bs-toggle="dropdown" aria-expanded="false">
-                            <i class="fa fa-bars"></i>
-                        </button>
-                        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="hamburgerMenu">
-                            <li><a class="dropdown-item" href="./?o=arak">Áraink</a></li>
-                            <li><a class="dropdown-item" href="#">Edzők</a></li>
-                            <li><a class="dropdown-item" href="#">Galéria</a></li>
-                            <li><a class="dropdown-item" href="#">Gyakori kérdések</a></li>
-                        </ul>
-                    </div>
-                </form>
-            </div>
-        </nav>
+        <?php include('oldalak/navbar.php'); ?>
+
         <div class="content">
         <?php
             if (isset($_GET['o'])) {
