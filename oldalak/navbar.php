78fd68cbfe78344d06338ddcaf2ca5211ad00288
odoroliver <71766082+odoroliver@users.noreply.github.com>
2024-10-28 19:17:52 +0100

    Update navbar.php

--- oldalak/navbar.php
+++ oldalak/navbar.php
@@ -1,4 +1,9 @@
-<nav class="navbar navbar-dark  bg-dark p-3">
+<?php
+$lang = isset($_SESSION['lang']) ? $_SESSION['lang'] : 'hu';
+$flagSrc = $lang === 'hu' ? 'images/hu_flag.png' : 'images/us_flag.png'; // Aktuális zászló
+?>
+
+<nav class="navbar navbar-dark bg-dark p-3">
     <div class="container-fluid">
         <a class="navbar-brand text-warning fs-2" href="./"><img src="images/logo.png" alt="LiftZone" title="LiftZone" class="logo"></a>
         <div class="ms-auto d-flex align-items-end">
@@ -10,7 +15,7 @@
                 <a class="btn btn-warning m-2" href="./?o=loginform">Bejelentkezés </a>
                 <a class="btn btn-warning m-2" href="./?o=registerform">Regisztráció</a>
             <?php else: ?>
-            <!-- Felhasználói profil információ -->
+                <!-- Felhasználói profil információ -->
                 <span class="navbar-text">
                     <a href="./?o=fiok" id="felhasznalo_nev" class="d-flex align-items-center">
                         <?= getUserInfo()[2] ?>
@@ -19,7 +24,25 @@
                 </span>
                 <a href="actions/logout.php" class="btn btn-danger m-2">Kijelentkezés</a>
             <?php endif; ?>
-            <!-- Hamburger ikon -->
+            <!-- Zászló gomb nyelvváltó menüvel -->
+            <div class="dropdown">
+                <button class="btn btn-dark dropdown-toggle d-flex align-items-center" type="button" id="languageMenu" data-bs-toggle="dropdown" aria-expanded="false">
+                    <img src="<?= $flagSrc ?>" alt="Language Flag" class="flag-icon" id="selectedFlag">
+                </button>
+                <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="languageMenu">
+                    <li id="hungaryOption">
+                        <a class="dropdown-item d-flex align-items-center" href="actions/change_language.php?lang=hu" onclick="switchFlag('images/hu_flag.png')">
+                            <img src="images/hu_flag.png" alt="Hungary" class="flag-icon"> Magyar
+                        </a>
+                    </li>
+                    <li id="usaOption">
+                        <a class="dropdown-item d-flex align-items-center" href="actions/change_language.php?lang=en" onclick="switchFlag('images/us_flag.png')">
+                            <img src="images/us_flag.png" alt="USA" class="flag-icon"> Angol
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <!-- Hamburger ikon oldalak menüvel -->
             <div class="dropdown">
                 <button class="btn btn-dark dropdown-toggle" type="button" id="hamburgerMenu" data-bs-toggle="dropdown" aria-expanded="false">
                     <i class="fa fa-bars"></i>
@@ -28,7 +51,7 @@
                     <li><a class="dropdown-item" href="./?o=arak">Áraink</a></li>
                     <li><a class="dropdown-item" href="#">Edzők</a></li>
                     <li><a class="dropdown-item" href="#">Galéria</a></li>
-                    <li><a class="dropdown-item" href="#">Gyakori kérdések</a></li>
+                    <li><a class="dropdown-item" href="./?o=faqs">Gyakran ismételt kérdések</a></li>
                 </ul>
             </div>
         </form>
