e2c8bee1ef36acbfe4e2b5a166ea578bcfaafe19
Olivér <scrapyvagyok@gmail.com>
2024-11-04 13:21:01 +0100

    translate, prices loop

--- oldalak/arak.php
+++ oldalak/arak.php
@@ -1,113 +1,45 @@
 <div class="arak">
-<img src="images/bg.jpg" alt="as">
-    <h1 class="text-center text-warning">Bérletárak</h1>
+    <img src="images/bg.jpg" alt="as">
+    <h1 class="text-center text-warning"><?= $languageContent["berletarak"] ?></h1>
     <div class="row">
         <div class="col-md-1"></div>
+        
+        <!-- Felnőtt -->
         <div class="col-md-5">
-            <h2 class=" text-warning section-title">Felnőtt</h2>
+            <h2 class="text-warning section-title"><?= $languageContent["felnottek"] ?></h2>
             <table class="table table-borderless">
                 <tr>
-                    <th>Típus</th>
-                    <th>Ár</th>
+                    <th><?= $languageContent["tipus"] ?></th>
+                    <th><?= $languageContent["ar"] ?></th>
                 </tr>
-                <tr>
-                    <td>Havi bérlet</td>
-                    <td>19 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Napijegy</td>
-                    <td>2 990 Ft</td>
-                </tr>
-                <tr>
-                    <td>Három hónapos bérlet</td>
-                    <td>49 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Hat hónapos bérlet</td>
-                    <td>99 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Éves bérlet</td>
-                    <td>199 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Egy hetes bérlet</td>
-                    <td>12 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Tíz alkalmas bérlet*</td>
-                    <td>17 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Délelőtti bérlet</td>
-                    <td>16 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Hétvégi bérlet</td>
-                    <td>12 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Törölköző használat (Kicsi)</td>
-                    <td>350 Ft</td>
-                </tr>
-                <tr>
-                    <td>Törölköző használat (Nagy)</td>
-                    <td>450 Ft</td>
-                </tr>
+                <?php foreach ($languageContent["berletek"] as $berlet): ?>
+                    <tr>
+                        <td><?= $berlet["tipus"] ?></td>
+                        <td><?= $berlet["felnottek_ar"] ?></td>
+                    </tr>
+                <?php endforeach; ?>
             </table>
         </div>
 
+        <!-- Diák / Nyugdíjas -->
         <div class="col-md-5">
-            <h2 class="text-warning section-title">Diák / Nyugdíjas</h2>
+            <h2 class="text-warning section-title"><?= $languageContent["diak_nyugdijas"] ?></h2>
             <table class="table table-borderless">
                 <tr>
-                    <th>Típus</th>
-                    <th>Ár</th>
+                    <th><?= $languageContent["tipus"] ?></th>
+                    <th><?= $languageContent["ar"] ?></th>
                 </tr>
-                <tr>
-                    <td>Havi bérlet</td>
-                    <td>17 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Napijegy</td>
-                    <td>2 490 Ft</td>
-                </tr>
-                <tr>
-                    <td>Három hónapos bérlet</td>
-                    <td>45 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Hat hónapos bérlet</td>
-                    <td>89 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Éves bérlet</td>
-                    <td>176 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Egy hetes bérlet</td>
-                    <td>11 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Tíz alkalmas bérlet*</td>
-                    <td>15 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Délelőtti bérlet</td>
-                    <td>15 900 Ft</td>
-                </tr>
-                <tr>
-                    <td>Törölköző használat (Kicsi)</td>
-                    <td>350 Ft</td>
-                </tr>
-                <tr>
-                    <td>Törölköző használat (Nagy)</td>
-                    <td>450 Ft</td>
-                </tr>
+                <?php foreach ($languageContent["berletek"] as $berlet): ?>
+                    <tr>
+                        <td><?= $berlet["tipus"] ?></td>
+                        <td><?= $berlet["diak_nyugdijas_ar"] ?></td>
+                    </tr>
+                <?php endforeach; ?>
             </table>
         </div>
+        
         <div class="col-md-1"></div>
     </div>
 
-    <p><i>*Az alkalmas bérletek a vásárlástól számított 60 napig érvényesek.</i></p>
+    <p><i><?= $languageContent["megjegyzes"] ?></i></p>
 </div>
--- oldalak/loginform.php
+++ oldalak/loginform.php
@@ -1,14 +1,14 @@
 <form class='form bg-dark text-light p-4 mt-4' action='actions/loginaction.php' method='post' target='kisablak'>
-    <h1 class='text-warning'>Bejelentkezés</h1>
+    <h1 class='text-warning'><?= $languageContent["login"]?></h1>
     <div id='error-message' class='alert alert-danger' style='display: none;'></div>
     <div class='mb-3' >
-        <label for='InputUsername' class='form-label'>Felhasználónév/email</label>
+        <label for='InputUsername' class='form-label'><?= $languageContent["nameOrEmail"]?></label>
         <input type='text' class='form-control form-control-dark' id='InputUsername' name='username'>
     </div>
     <div class='mb-3' id='password-container'>
-        <label for='InputPassword' class='form-label'>Jelszó</label>
+        <label for='InputPassword' class='form-label'><?= $languageContent["pw"]?></label>
         <input type='password' class='form-control form-control-dark' id='InputPassword' name='password'>
         <img style='margin-top:17px;' src='images/hidden.png' id='eyeIcon' class='eye-icon' alt='Show/Hide Password'>
     </div>
-    <button type='submit' class='btn btn-warning w-100'>Bejelentkezek</button>
+    <button type='submit' class='btn btn-warning w-100'><?= $languageContent["login"]?></button>
 </form>
