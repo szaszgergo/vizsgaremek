989b8d6c0779fd9db8480084061ef09c70cf6d3b
Fr-y <gaborkorpa@gmail.com>
2024-11-05 20:30:54 +0100

    Statisztika oldal, kezdetleges

--- oldalak/admin/statisztika.php
+++ oldalak/admin/statisztika.php
@@ -1,4 +1,12 @@
+<h1>Legtöbbet látogatott oldalak:</h1>
 <?php
-//naplo lekerdezes es statisztika melyik oldalt latogatjak legtobbet
+$oldalak = sqlcall("SELECT nURL, COUNT(nURL) as count FROM `naplo` GROUP BY nURL ORDER BY count DESC;");
+while ($row = $oldalak->fetch_assoc()):?>
+<div class="row">
+    <div class="col-md-6"><p><?php echo $row['nURL'];?></p></div>
+    <div class="col-md-6"><p><?php echo $row['count'];?> látogatás</p></div>
+</div>
 
-?>
+
+<?php endwhile; ?>
+
