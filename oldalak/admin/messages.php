2a79d3778a1c9076033f9003d4e03a7d42aad0df
Fr-y <gaborkorpa@gmail.com>
2024-11-14 14:56:51 +0100

    styling

--- oldalak/admin/messages.php
+++ oldalak/admin/messages.php
@@ -5,9 +5,9 @@
 <?php
 $oldalak = sqlcall("SELECT * FROM `messages` ORDER BY mDate DESC;");
 while ($row = $oldalak->fetch_assoc()):?>
-<div class="row">
-    <div class="col-md-6"><p><?php echo $row['mEmail'];?></p></div>
-    <div class="col-md-6"><p><?php echo $row['mUzenet'];?></p></div>
-</div>
 
+    <div class="table row">
+        <div class="col-md-3"><p><?php echo $row['mEmail'];?></p></div>
+        <div class="col-md-9"><p><?php echo $row['mUzenet'];?></p></div>
+    </div>
 <?php endwhile; ?>
