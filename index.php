317e1991147a006d85bf173feac399c1cc423309
Fr-y <gaborkorpa@gmail.com>
2024-11-18 12:14:55 +0100

    refactor

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 
-//requireok
 require('actions/apicall.php');
 require('actions/getuserinfo.php');
 require('actions/getjegytipusadatok.php');
-//bövithető lista az uj oldalak cimeihez 
+
 $cimek = [
     "arak" => "Áraink - LiftZone",
     "fiok" => "Fiókod - LiftZone",
@@ -15,11 +14,12 @@ $cimek = [
     "adatvedelem" => "Adatvédelem - LiftZone",
     "faqs" => "Gyakran ismételt kérdések - LiftZone",
     "hazirend" => "Házirend - LiftZone",
-    "edzok"=> "Személyi Edzőink - LiftZone",
-    "galeria"=> "Galéria - LiftZone",
+    "edzok" => "Személyi Edzőink - LiftZone",
+    "galeria" => "Galéria - LiftZone",
     "admin" => "Admin Kezelőfelület  - LiftZone",
     "" => "Főoldal - LiftZone",
 ];
+
 $belepettoldalak = [
     "arak" => "oldalak/arak.php",
     "fiok" => "oldalak/fiok.php",
@@ -28,8 +28,8 @@ $belepettoldalak = [
     "adatvedelem" => "oldalak/adatvedelem.php",
     "faqs" => "oldalak/faqs.php",
     "hazirend" => "oldalak/hazirend.php",
-    "edzok"=> "oldalak/edzok.php",
-    "galeria"=> "oldalak/galeria.php",
+    "edzok" => "oldalak/edzok.php",
+    "galeria" => "oldalak/galeria.php",
     "admin" => "oldalak/admin.php",
     "" => "oldalak/main.php",
 ];
@@ -42,113 +42,111 @@ $oldalak = [
     "faqs" => "oldalak/faqs.php",
     "hazirend" => "oldalak/hazirend.php",
     "admin" => "oldalak/admin.php",
-    "edzok"=> "oldalak/edzok.php",
-    "galeria"=> "oldalak/galeria.php",
+    "edzok" => "oldalak/edzok.php",
+    "galeria" => "oldalak/galeria.php",
     "" => "oldalak/main.php",
 ];
 
-$o = isset($_GET['o']) ? $_GET['o'] : "";
-if (array_key_exists($o, $cimek)) {
-    $cim = $cimek[$o];
-} else {
-    $cim = "404 - LiftZone";
-}
-
+$o = $_GET['o'] ?? "";
+$cim = $cimek[$o] ?? "404 - LiftZone";
 
 ?>
 
 <!DOCTYPE html>
 <html lang="hu">
+
 <head>
-<!-- <meta name="robots" content="noindex"> majd adminba --> 
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css" />
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" 
+          integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://unpkg.com/leaflet/dist/leaflet.css">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <link rel="stylesheet" href="style.css">
-
     <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon">
-
-    <title><?=$cim?></title>
+    <title><?= htmlspecialchars($cim) ?></title>
 </head>
+
 <body>
     <div class="site">
-        <?php require('actions/naplozas.php'); naplo(); ?>
-        <?php include('actions/language.php'); ?>
-        <?php require('oldalak/navbar.php'); ?>
-
+        <?php
+        require('actions/naplozas.php');
+        naplo();
+        include('actions/language.php');
+        require('oldalak/navbar.php');
+        ?>
 
         <div class="content">
-            
-        <?php
-            $belepett = isset($_SESSION["uid"]);
-
-            //belepett vagy a sima oldalak listajat hasznaljuk
+            <?php
+            $belepett = !empty($_SESSION["uid"]);
             $pages = $belepett ? $belepettoldalak : $oldalak;
 
-            //link alapjan require
-            if (array_key_exists($o, $pages)) {
+            if (isset($pages[$o])) {
                 require($pages[$o]);
             } else {
                 require("oldalak/404.php");
             }
-            //debug kiiratás
+
             print_r($_SESSION);
             ?>
-    
         </div>
 
         <?php require('oldalak/footer.php'); ?>
-
     </div>
+
     <iframe name='kisablak' style="display: none;"></iframe>
 
+    <?php
+    $popups = [
+        'jegyvasarlasform' => [
+            "oldalak/popups/jegy_vasarlas_popup.php",
+            "oldalak/popups/kartya_informacio_popup.php"
+        ],
+        'fiok' => [
+            "oldalak/admin/popups/vasarlasi_elozmeny_popup.php"
+        ]
+    ];
 
-    <?php require("oldalak/jegyvasarlaspopup.php")?>
-    <?php require("oldalak/kartyaformpopup.php")?>
-    <?php if (isset($_GET['a'])) {
-    require("oldalak/admin/popups/uj_kupon.php");
-    } ?>
+    $adminPopups = [
+        'a' => "oldalak/admin/popups/uj_kupon.php"
+    ];
 
+    if (isset($popups[$o])) {
+        foreach ($popups[$o] as $popup) {
+            require($popup);
+        }
+    }
+
+    if (isset($_GET['a'], $adminPopups['a'])) {
+        require($adminPopups['a']);
+    }
+    ?>
 
     <script>
-        //majd ezt a hibakezelést is megcsinálom -gabor
-        window.addEventListener('message', function(event) {
-            if (event.data.loginError) {
-                document.getElementById('error-message').innerHTML = event.data.loginError;
-                document.getElementById('error-message').style.display = 'block';
-            }
-            if (event.data.loginSuccess) {
-                document.getElementById('error-message').style.display = 'none';
+        window.addEventListener('message', function (event) {
+            const data = event.data;
+            const errorMessage = document.getElementById('error-message');
+
+            if (data.loginError) {
+                errorMessage.innerHTML = data.loginError;
+                errorMessage.style.display = 'block';
+            } else if (data.loginSuccess) {
+                errorMessage.style.display = 'none';
                 window.location.href = "./?o=fiok";
-            }
-            if (event.data.Success) {
-                window.location.href = "./";}
-            if (event.data.editError) {
-                document.getElementById('error-message').innerHTML = event.data.editError;
-                document.getElementById('error-message').style.display = 'block';
-            }
-            if (event.data.editSuccess) {
-                document.getElementById('error-message').style.display = 'none';
-                window.location.href = "./?o=admin";
-            }
-            if (event.data.purchaseSuccess) {
+            } else if (data.purchaseSuccess) {
                 window.location.href = "./?o=fiok";
-            }
-            if (event.data.purchaseError) {
-                document.getElementById('error-message').innerHTML = event.data.updateError;
-                document.getElementById('error-message').style.display = 'block';
-            }
-            if (event.data.regSuccess) {
-                document.getElementById('error-message').style.display = 'none';
+            } else if (data.purchaseError) {
+                errorMessage.innerHTML = data.updateError;
+                errorMessage.style.display = 'block';
+            } else if (data.regSuccess) {
+                errorMessage.style.display = 'none';
                 window.location.href = "./?o=loginform";
                 alert('Sikeres regisztráció, lépj be!');
             }
         });
     </script>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" 
+            integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
     <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
     <script src="js/pic_change.js"></script>
@@ -156,8 +154,6 @@ if (array_key_exists($o, $cimek)) {
     <script src="js/language.js"></script>
     <script src="js/sponsor-slide.js"></script>
     <script src="js/edit.js"></script>
-
-
-
 </body>
+
 </html>
