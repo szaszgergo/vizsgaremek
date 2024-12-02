9bcbc30b863759b240c22800b696a1b6e3fe1b96
Olivér <scrapyvagyok@gmail.com>
2024-12-02 14:54:03 +0100

    kommentek uj oldalon huszasaval

--- oldalak/admin/velemenyek.php
+++ oldalak/admin/velemenyek.php
@@ -1,6 +1,3 @@
-<div class="row">
-    <div class="col-md-12"><p><h3><b>Vélemények</b></h3></p></div>
-</div>
 <?php
 $oldalak = sqlcall("
     SELECT edzok_kommentek.ekID, edzok_kommentek.ekKomment, edzok_kommentek.ekDatum, user.uemail
@@ -8,27 +5,37 @@ $oldalak = sqlcall("
     INNER JOIN user ON edzok_kommentek.ekUserID = user.uID
     ORDER BY edzok_kommentek.ekDatum DESC;
 ");
-
-while ($row = $oldalak->fetch_assoc()): ?>
-    <div class="table row">
-        <div class="col-md-10">
-            <p><strong>Email:</strong> <?php echo $row['uemail']; ?></p>
-            <p><strong>Komment:</strong> <?php echo $row['ekKomment']; ?></p>
-            <p><small><em>Dátum: <?php echo $row['ekDatum']; ?></em></small></p>
+?>
+<div id="all-comments" style="display: none;">
+    <?php while ($row = $oldalak->fetch_assoc()): ?>
+        <div class="comment">
+            <div class="table row">
+                <div class="col-md-10">
+                    <p><strong>Email:</strong> <?php echo $row['uemail']; ?></p>
+                    <p><strong>Komment:</strong> <?php echo $row['ekKomment']; ?></p>
+                    <p><small><em>Dátum: <?php echo $row['ekDatum']; ?></em></small></p>
+                </div>
+                <div class="col-md-1">
+                    <form method="post" action="actions/velemeny_feldolgozas.php" target="kisablak">
+                        <input type="hidden" name="ekID" value="<?php echo $row['ekID']; ?>">
+                        <input type="hidden" name="accept" value="elfogadas">
+                        <button type="submit" class="btn btn-success">✔</button>
+                    </form>
+                </div>
+                <div class="col-md-1">
+                    <form method="post" action="actions/velemeny_feldolgozas.php" target="kisablak">
+                        <input type="hidden" name="ekID" value="<?php echo $row['ekID']; ?>">
+                        <input type="hidden" name="deny" value="elutasitas">
+                        <button type="submit" class="btn btn-danger">✖</button>
+                    </form>
+                </div>
+            </div>
         </div>
-        <div class="col-md-1">
-            <form method="post" action="actions/velemeny_feldolgozas.php" target="kisablak">
-                <input type="hidden" name="ekID" value="<?php echo $row['ekID']; ?>">
-                <input type="hidden" name="accept" value="elfogadas">
-                <button type="submit" class="btn btn-success">✔</button>
-            </form>
-       </div>
-       <div class="col-md-1">
-            <form method="post" action="actions/velemeny_feldolgozas.php" target="kisablak">
-                <input type="hidden" name="ekID" value="<?php echo $row['ekID']; ?>">
-                <input type="hidden" name="deny" value="elutasitas">
-                <button type="submit" class="btn btn-danger">✖</button>
-            </form>
-       </div>
-    </div>
-<?php endwhile; ?>
+    <?php endwhile; ?>
+</div>
+
+<div id="comments-container"></div>
+
+<div id="pagination-controls" style="text-align: center; margin-top: 20px;"></div>
+
+<script src="js/kommentek.js"></script>
