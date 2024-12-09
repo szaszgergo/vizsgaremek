d137493113154d5dd21f27a206f2045d41458142
Olivér <scrapyvagyok@gmail.com>
2024-12-09 13:16:36 +0100

    uzenofal okes

--- actions/uzenofalFeldolgozas.php
+++ actions/uzenofalFeldolgozas.php
@@ -1,12 +1,23 @@
 <?php
 require("sqlcall.php");
+
 if(isset($_POST["cim"]) && isset($_POST["szoveg"])) {
     $cim = $_POST["cim"];
     $szoveg = $_POST["szoveg"];
     $curdate = date('Y-m-d H:i:s');
-    $sql = "INSERT INTO uzenofal (uzenoCim, uzenoSzoveg, uzenoDatum) VALUES (?, ?, ?)";
-    $params = [$cim, $szoveg, $curdate];
-    $types = "sss";
+    
+    $kep = null;
+    if(isset($_FILES["kep"]) && $_FILES["kep"]["error"] == 0) {
+        $target_dir = "C:/xampp/htdocs/vizsgaremek/images/uzenofal/";
+        $target_file = $target_dir . basename($_FILES["kep"]["name"]);
+        if(move_uploaded_file($_FILES["kep"]["tmp_name"], $target_file)) {
+            $kep = $target_file;
+        }
+    }
+
+    $sql = "INSERT INTO uzenofal (uzenoCim, uzenoSzoveg, uzenoDatum, uzenoKep) VALUES (?, ?, ?, ?)";
+    $params = [$cim, $szoveg, $curdate, $kep];
+    $types = "ssss";
     sqlsave($sql, $types, $params);
     header("Location: " . ($_SERVER['HTTP_REFERER'] ?? 'index.php'));
     exit();
