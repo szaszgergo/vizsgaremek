2ebd76996f28d9a5364fff58a8db82613933114f
Olivér <scrapyvagyok@gmail.com>
2025-01-08 13:20:40 +0100

    üzenőfal nincskép fix

--- actions/uzenofalFeldolgozas.php
+++ actions/uzenofalFeldolgozas.php
@@ -21,13 +21,20 @@ if (isset($_POST["cim"]) && isset($_POST["szoveg"])) {
     $szoveg = $_POST["szoveg"];
     $curdate = date('Y-m-d H:i:s');
 
-
-    $sql = "INSERT INTO uzenofal (uzenoCim, uzenoSzoveg, uzenoDatum, uzenoKep) VALUES (?, ?, ?, ?)";
-    $params = [$cim, $szoveg, $curdate, $kepnev];
-    $types = "ssss";
+    if ($vankep) {
+        $sql = "INSERT INTO uzenofal (uzenoCim, uzenoSzoveg, uzenoDatum, uzenoKep) VALUES (?, ?, ?, ?)";
+        $params = [$cim, $szoveg, $curdate, $kepnev];
+        $types = "ssss";
+      
+        $result = sqlsave($sql, $types, $params);
+    }
+    else {
+        $sql = "INSERT INTO uzenofal (uzenoCim, uzenoSzoveg, uzenoDatum) VALUES (?, ?, ?)";
+        $params = [$cim, $szoveg, $curdate];
+        $types = "sss";
   
-    $result = sqlsave($sql, $types, $params);
-
+        $result = sqlsave($sql, $types, $params);
+    }
     if ($result) {
         echo "<script>window.top.postMessage({success: 'A közzététel sikeres volt!'}, '*');</script>";
 
