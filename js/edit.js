8a0d20d0cbede89656ff1f97c46823b3207d7be5
Olivér <scrapyvagyok@gmail.com>
2025-01-20 13:59:33 +0100

    edit to save; pagination on messages

--- js/edit.js
+++ js/edit.js
@@ -9,10 +9,31 @@ editButtons.forEach(editButton => {
 
         var inputs = row.querySelectorAll('input');
         inputs.forEach(input => {
+            input.setAttribute('data-original-value', input.value);
+
             input.removeAttribute('readonly');
-            input.removeAttribute('disabled');
             input.style.backgroundColor = '#fff';
             input.style.color = '#000';
+        });
+    }
+});
+
+var saveButtons = document.querySelectorAll("#btn-save");
+
+saveButtons.forEach(saveButton => {
+    saveButton.onclick = function () {
+        var row = saveButton.closest("#inputcontainer");
+        var editButton = row.querySelector("#edit-btn");
+        editButton.style.display = 'block';
+        saveButton.style.display = 'none';
+
+        var inputs = row.querySelectorAll('input');
+        inputs.forEach(input => {
+            input.value = input.getAttribute('data-original-value');
+
+            input.setAttribute('readonly', 'true');
+            input.style.backgroundColor = "transparent";
+            input.style.color = '#fff';
         });
     }
 });
