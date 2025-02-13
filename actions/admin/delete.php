35322751dc5944547a2b14b440a88b58b9433252
Fr-y <gaborkorpa@gmail.com>
2025-02-13 19:02:34 +0100

    termék kezelés javitasok talan mukodnek

--- actions/admin/delete.php
+++ actions/admin/delete.php
@@ -1,9 +1,24 @@
 <?php
     require('../sqlcall.php');
-    require("../formhandling.php");
-
-    $uid = intval($_POST['uid']);
-    echo $_POST['uid'];
-    sqlsave("UPDATE user SET uStatus = 'Deleted' WHERE uID = $uid");
-    echo "<script>window.top.postMessage({editSuccess: true}, '*');</script>";
+    
+    function dinamikusSQLGenerátor_inátor_csak_torleshez($tabla, $primaryKey, $id, $statusField) {
+        $tabla = preg_replace('/[^a-zA-Z0-9_]/', '', $tabla);
+        $primaryKey = preg_replace('/[^a-zA-Z0-9_]/', '', $primaryKey);
+        $statusField = preg_replace('/[^a-zA-Z0-9_]/', '', $statusField);
+        
+        $id = intval($id);
+        
+        $sql = "UPDATE $tabla SET $statusField = 0 WHERE $primaryKey = $id";
+        return $sql;
+    }
+    
+    $tabla = $_POST['tabla'];
+    $primaryKey = $_POST['primary_key'];
+    $id = intval($_POST['id']);
+    $statusField =$_POST['status'] ;
+    
+    $sql = dinamikusSQLGenerátor_inátor_csak_torleshez($tabla, $primaryKey, $id, $statusField);
+    sqlsave($sql);
+    
+    echo "<script>window.top.postMessage({deleteSuccess: true}, '*');</script>";
 ?>
