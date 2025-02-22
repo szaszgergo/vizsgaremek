affdf61e20643f6ae3dbd65febe2a896a24913aa
Fry <58634993+Fr-y@users.noreply.github.com>
2025-02-22 22:17:25 +0100

    Postra atiras mert a mobilhoz ugy jobb

--- api/login/index.php
+++ api/login/index.php
@@ -1,6 +1,7 @@
 <?php
     header('Content-Type: application/json; charset=utf-8');
     include("../../actions/sqlcall.php");
+
     function valaszKuldes($status, $uzenet, $data = []) {
         echo json_encode(array(
             'status' => $status,
@@ -10,41 +11,44 @@
         exit;
     }
 
-    //ha nincs megadva a paraméter küldés és kilépés
-    if (!isset($_GET['username']) || !isset($_GET['password'])) {
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        valaszKuldes(405, 'Csak POST kérés engedélyezett');
+    }
+
+    $inputJSON = file_get_contents('php://input');
+    $input = json_decode($inputJSON, true);
+
+    if (!isset($input['username']) || !isset($input['password'])) {
         valaszKuldes(400, 'Hiányzó paraméter: username, password');
     }
 
-    $name = $_GET['username'];
-    $password = $_GET['password'];
+    $name = $input['username'];
+    $password = $input['password'];
     
+    // SQL query to find user by username or email
     $sqllekerdezes = "SELECT uID, uPassword FROM user WHERE uFelhasznalonev = '$name' OR uemail = '$name'";
     $tabla = sqlcall($sqllekerdezes);
     
     $sor = $tabla->fetch_row();
-    //ha nincs result válasz és kilépés
-    if (!isset($sor)) {
+
+    if (!$sor) {
         valaszKuldes(404, 'Hibás bejelentkezési adatok');
     }
-    //ha nem egyezik a jelszó küldés és kilépés
+
     $hashedpassword = $sor[1];
-    if(!password_verify($password, $hashedpassword)){
+    if (!password_verify($password, $hashedpassword)) {
         valaszKuldes(404, 'Hibás jelszó');
     }
 
-    //ha ide eljutott akkor minden fain, visszaküldjük az id-t és ezt majd az oldalon vagy a mobilon sessionbe rakjuk
     $valasz = [
         'uID' => $sor[0]
     ];
-
     valaszKuldes(200, 'A bejelentkezés megtörtént!', $valasz);
 
-    $curdate  = date('Y-m-d h:i:s', time());
+    $curdate  = date('Y-m-d H:i:s');
     $ip = $_SERVER['REMOTE_ADDR'];
     $sessionid = session_id();
-    $url = $_SERVER['REQUEST_URI'];
-    $sql = "INSERT INTO login (lID, lDatum, lIP, lSession, luID) VALUES
-    ('', '$curdate', '$ip', '$sessionid', '$sor[0]')";
-    sqlsave($sql);
 
+    $sql = "INSERT INTO login (lDatum, lIP, lSession, luID) VALUES (?, ?, ?, ?)";
+    sqlsave($sql, [$curdate, $ip, $sessionid, $sor[0]]);
 ?>
