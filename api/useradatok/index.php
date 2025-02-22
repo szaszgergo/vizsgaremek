c054713ad55b3470286f6a90729546566766708f
Fr-y <gaborkorpa@gmail.com>
2025-02-22 22:45:28 +0100

    aktiv jegy visszakuldese

--- api/useradatok/index.php
+++ api/useradatok/index.php
@@ -40,8 +40,16 @@
         $jegyekarray[] = $row;
     }
 
+    $aktivjegysql = "SELECT * FROM jegyek WHERE juID = '$uid' AND jStatus = 1";
+    $aktivjegy = sqlcall($aktivjegysql)->fetch_assoc();
+    $aktivjegyneve = getJegyTipusAdatok($aktivjegy['jtID'])['tpNev'];
+
+
+
+
     $valasz = [
         'user' => $useradatok,
+        'aktivjegy' => [$aktivjegy, $aktivjegyneve],
         'jegyek' => $jegyekarray
     ];
 
