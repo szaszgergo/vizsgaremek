7fb855065cefd9cde238663f7fa31ca1a38e4d80
Fr-y <gaborkorpa@gmail.com>
2025-02-24 17:45:58 +0100

    jegy vásárlás és aktiválás api

---
+++ api/jegyaktivalas/index.php
@@ -0,0 +1,39 @@
+<?php
+    header('Content-Type: application/json; charset=utf-8');
+    include("../../actions/sqlcall.php");
+    function valaszKuldes($status, $uzenet, $data = []) {
+        echo json_encode(array(
+            'status' => $status,
+            'uzenet' => $uzenet,
+            'data' => $data
+        ), JSON_UNESCAPED_UNICODE);
+        exit;
+    }
+
+    if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+        valaszKuldes(405, 'Csak POST kérés engedélyezett');
+    }
+
+    $inputJSON = file_get_contents('php://input');
+    $input = json_decode($inputJSON, true);
+
+    if (!isset($input['uID']) || !isset($input['jID'])) {
+        valaszKuldes(400, 'Hiányzó paraméter: uID, jID');
+    }
+
+    $uID = $input['uID'];
+    $jID = $input['jID'];
+
+    $datum = date("Y-m-d H:i:s");
+
+    $jtid = sqlcall("SELECT jtID FROM jegyek WHERE jID = $jID")->fetch_assoc()["jtID"];
+    $hossz = sqlcall("SELECT tpHossz FROM tipusok WHERE tpID = $jtid")->fetch_assoc()["tpHossz"];
+    
+    $lejarat = date("Y-m-d H:i:s", strtotime($datum . " + $hossz days"));
+    
+    $sql = "UPDATE jegyek SET jStatus = 1, jKezdes = '$datum', jLejarat = '$lejarat' WHERE jID = $jID AND juID = $uID";
+    sqlcall($sql);
+
+    valaszKuldes(200, 'Sikeres aktiválás');
+
+?>
