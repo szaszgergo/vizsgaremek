e946f9ddb69a2662da67ee46cb73b14334c2a0ad
Olivér <scrapyvagyok@gmail.com>
2025-03-01 15:10:01 +0100

    .

--- oldalak/fiok/felhasznalo_adatok.php
+++ oldalak/fiok/felhasznalo_adatok.php
@@ -75,9 +75,7 @@ if (isset($_SESSION["2fa_uid"])) {
 <form action="actions/enable_2fa.php" method="POST" target="kisablak">
     <button type="submit" class="btn btn-success" <?= ($on===true) ? "disabled" : "" ?>>Kétlépcsős azonosítás bekapcsolása</button>
 </form>
-<!-- ($on===false) ? "disabled" : ""  -->
-<!-- action="actions/disable_2fa.php" method="POST" target="kisablak" -->
  <div>
-    <button type="submit" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#login2fa">Kétlépcsős azonosítás kikapcsolása</button>
+    <button type="submit" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#login2fa" <?= ($on===false) ? "disabled" : "" ?>>Kétlépcsős azonosítás kikapcsolása</button>
  </div>
   
